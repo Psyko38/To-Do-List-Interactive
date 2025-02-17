bed8d9aa7978b2e55533d6c72ccb9db3b912fa0c
Psyko38 <110417117+Psyko38@users.noreply.github.com>
2025-02-17 14:51:16 +0100

    Step 3

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const AddBTN = document.querySelector(".form > button");
 const TextInput = document.getElementById("task");
 const TaskDiv = document.getElementById("Taskdiv");
+const socreSPan = document.getElementById("socreSPan");
+const MaxValueSpan = document.getElementById("MaxValueSpan");
 
 AddBTN.addEventListener("click", () => {
 	AddElemnt(TextInput.value);
+	TextInput.value = "";
 });
 
 function AddElemnt(Name) {
@@ -15,11 +18,23 @@ function AddElemnt(Name) {
 	ChekBox.id = "check";
 	P.innerText = Name;
 	BTN.innerText = "X";
+	MaxValueSpan.innerText = parseInt(MaxValueSpan.innerText) + 1;
 	TaskDiv.appendChild(MainDiv);
 	MainDiv.appendChild(ChekBox);
 	MainDiv.appendChild(P);
 	MainDiv.appendChild(BTN);
 	BTN.addEventListener("click", () => {
 		MainDiv.remove();
+		MaxValueSpan.innerText = parseInt(MaxValueSpan.innerText) - 1;
+		if (ChekBox.checked) {
+			socreSPan.innerText = parseInt(socreSPan.innerText) - 1;
+		}
+	});
+	ChekBox.addEventListener("change", () => {
+		if (ChekBox.checked) {
+			socreSPan.innerText = parseInt(socreSPan.innerText) + 1;
+		} else {
+			socreSPan.innerText = parseInt(socreSPan.innerText) - 1;
+		}
 	});
 }
